8926ae16316e70ef2745fe4b65bd790873ea6939
chucthang2408 <minhtienn2408@gmail.com>
2021-09-17 21:33:55 +0700

    push index.php

--- index.php
+++ index.php
@@ -8,28 +8,16 @@
 </head>
 <body>
     <div class="table">
-    Admin@DESKTOP-FT1E413 MINGW64 ~/Desktop/testwithTien/tesgit (daidev)
-$ git push origin daidev
-Enumerating objects: 7, done.
-Counting objects: 100% (7/7), done.
-Delta compression using up to 4 threads
-Compressing objects: 100% (3/3), done.
-Writing objects: 100% (4/4), 355 bytes | 118.00 KiB/s, done.
-Total 4 (delta 1), reused 0 (delta 0), pack-reused 0
-remote: Resolving deltas: 100% (1/1), completed with 1 local object.
-remote: 
-remote: Create a pull request for 'daidev' on GitHub by visiting:
-remote:      https://github.com/chucthang2408/tesgit/pull/new/daidev
-remote:
-To https://github.com/chucthang2408/tesgit.git
- * [new branch]      daidev -> daidev
-
-Admin@DESKTOP-FT1E413 MINGW64 ~/Desktop/testwithTien/tesgit (daidev)
+        okok
+    </div>
     <div class="mt-container">
         <div class="mt-test">
             <div class="mt-test">
             thawfng tien lon que
             </div>
+        </div>
+        <div class="table">
+            okok
         </div>
     </div>
 </body>
